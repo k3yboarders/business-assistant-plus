9a916ba4d32aba29c554234f2bf4eee9200c83c6
Tomasz Kulig <pewienjaaa@gmail.com>
2022-08-10 17:00:52 +0200

    🚁 Connect chart with NBP

--- scripts/charts.js
+++ scripts/charts.js
@@ -1,15 +1,20 @@
+const currencies = ['EUR', 'USD'];
+
+let urlBase = 'https://api.nbp.pl/api/exchangerates/rates/a';
+
 const ctx = document.getElementsByClassName('chart')[0].getContext('2d');
 const currencyChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['EUR', 'USD'],
+        labels: currencies,
         datasets: [{
             label: 'W przeliczeniu na złoty',
-            data: [4.71, 4.62],
+            data: [],
             backgroundColor: [
                 'rgb(255, 100, 100)',
                 'rgb(100, 255, 100)',
-            ]
+            ],
+            borderRadiues: 3
         }]
     },
     options: {
@@ -20,3 +25,14 @@ const currencyChart = new Chart(ctx, {
         }
     }
 })
+
+for (const cur of currencies) {
+    fetch(`${urlBase}/${cur}`)
+    .then(res => res.json())
+    .then((out) => {
+        currencyChart.data.datasets[0].data.push(out.rates[0].mid);
+        currencyChart.update();
+    })  
+};
+
+
