5eab34bc4286662642010ff91547238b7d3723a7
Seweryn Pajor <archdevx7d6@sewe-hub.xyz>
2022-08-13 19:44:36 +0200

    Fix compilation errors and warnings

--- Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -6,8 +6,6 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
