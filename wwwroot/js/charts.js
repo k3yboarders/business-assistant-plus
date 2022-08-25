c8f118693b08f13c56561f06fbf80fc9b749a886
cooligus <contact@tkulig.com>
2022-08-25 13:06:07 +0200

    🧱 Create basics for more advanced charts

--- wwwroot/js/charts.js
+++ wwwroot/js/charts.js
@@ -1,18 +1,47 @@
-const baseCurrency = 'EUR';
-const toExchange = 'PLN';
+let baseCurrency = 'EUR';
+let toExchange = 'PLN';
+const chartPeroids = ['W', 'M', '3M', '6M', 'Y', '5Y', 'Max'];
+const chartMaxSamples = 100;
 
-let url = `https://api.exchangerate.host/timeseries?start_date=2019-01-01&end_date=2021-01-01&symbols=${toExchange}&base=${baseCurrency}`;
+const getExchangeRate = (peroidIndex) => {
+    let now = luxon.DateTime.now();
+
+    switch(peroidIndex) {
+    case 0: now = now.minus({weeks:1}); break;
+    case 1: now = now.minus({months:1}); break;
+    case 2: now = now.minus({months:3}); break;
+    case 3: now = now.minus({months:6}); break;
+    case 4: now = now.minus({years:1}); break;
+    case 5: now = now.minus({years:5}); break;
+    default: now = luxon.DateTime.fromISO('2000-01-01');
+    }
+
+    let url = `https://api.exchangerate.host/timeseries?start_date=${now.toISODate()}&end_date=${luxon.DateTime.now().toISODate()}&symbols=${toExchange}&base=${baseCurrency}`;
+    console.log(url);
+
+    fetch(`${url}`)
+        .then(res => res.json())
+        .then((out) => {
+            const rates = Object.entries(out.rates);
+            for(let i = 0; i <  rates.length; i++) {
+                currencyChart.data.datasets[0].data.push({x: Date.parse(rates[i][0]), y: rates[i][1].PLN});
+            }
+            currencyChart.update();
+        });
+}
 
 const ctx = document.getElementsByClassName('chart')[0].getContext('2d');
 const gradient = ctx.createLinearGradient(0,0,0,400);
 gradient.addColorStop(0, 'rgba(255,255,0,.3)');
 gradient.addColorStop(1, 'rgba(255,255,0,0)');
+
+getExchangeRate(0);
+
 const currencyChart = new Chart(ctx, {
 
   type: 'line',
   data: {
     datasets: [{
-        label: `Koszt ${baseCurrency} w ${toExchange}`,
         data: [],
         fill: true,
         borderColor: 'rgba(155,155,0,.8)',
@@ -23,12 +52,10 @@ const currencyChart = new Chart(ctx, {
     ]
   },
   options: {
-    parsing: false,
-    plugins: {
+      parsing: false,
+      plugins: {
       legend: {
-        labels: {
-          boxWidth: 0,
-        }
+        display: false
       },
       decimation: {
         algorithm: 'lttb',
@@ -62,12 +89,3 @@ const currencyChart = new Chart(ctx, {
   }
 })
 
-fetch(`${url}`)
-.then(res => res.json())
-.then((out) => {
-    const rates = Object.entries(out.rates);
-    for(let i = 0; i <  rates.length; i++) {
-        currencyChart.data.datasets[0].data.push({x: Date.parse(rates[i][0]), y: rates[i][1].PLN});
-    }
-    currencyChart.update();
-});
