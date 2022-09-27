16d31f3e737a0b0b15d0668bb79aaa8726c525f8
maxidragon <mgwspinanie2006@wp.pl>
2022-09-27 07:49:16 +0200

    Migration

---
+++ Migrations/20220927053916_FavouriteExchange.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMP.Migrations
+{
+    public partial class FavouriteExchange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FavouriteExchange",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FavouriteExchangeBase",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FavouriteExchange",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FavouriteExchangeBase",
+                table: "AspNetUsers");
+        }
+    }
+}
